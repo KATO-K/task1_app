4497477c54166fa2466d4b114951eb56d4cd6844
rayzymoon <rayzymoon@yahoo.co.jp>
2022-05-28 15:41:59 +0900

    step1-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  devise_scope :user do
-    root "users/sessions#new"
-  end
-  
+
   get 'home/top'
-  devise_for :users,
-  controllers: {
-      
-      confirmations: 'users/confirmations'
-      
-    }
+  get 'users/dash_boards'
 
   devise_scope :user do
+    root "users/sessions#new"
+
     put 'confirmation', to: 'users/confirmations#show', as: :back_confirmation
   end
+
+  devise_for :users,
+  controllers: {
+      confirmations: 'users/confirmations',
+      dashboards: 'users/dash_boards',
+    }
+
+  
 end
