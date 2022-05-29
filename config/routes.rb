c21488fb4001eb486342a3b751f07729cd3e0818
rayzymoon <rayzymoon@yahoo.co.jp>
2022-05-29 11:59:58 +0900

    step1-6

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'articles/new'
+  get 'articles/create'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
   get 'home/top'
-  get 'users/dash_boards'
+  # get 'users/dash_boards'
 
   devise_scope :user do
     root "users/sessions#new"
@@ -15,6 +17,11 @@ Rails.application.routes.draw do
       confirmations: 'users/confirmations',
       dashboards: 'users/dash_boards',
     }
+
+  namespace :users do
+    resources :dash_boards, only: [:index]
+    resources :articles
+  end
 
   
 end
